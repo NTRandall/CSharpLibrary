aa0bb111e31d4eaa4613a5897ad0a148e4829101
NTRandall <NathanTRandall@gmail.com>
2017-03-22 13:45:45 -0400

    strings conditionals and more

---
+++ 0.03_strings/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._03_strings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //name demo
+
+            //string firstName;
+            //string lastName;
+            //string occupation;
+
+            //firstName = "Zakk";
+            //lastName = "Wilde";
+            //occupation = "Bringing Metal to the Children";
+            //string fullName = firstName + " " + lastName;
+            //Console.WriteLine(fullName);
+
+            //String Interpolation
+            //Console.WriteLine("Name: {0}" + "\n" + "Occupation: {1}", fullName, occupation);
+            //Console.ReadLine();
+
+            string dog;
+            string cat;
+
+            dog = "McGreggor";
+            cat = "Lilly";
+
+            string dogowner;
+            string catowner;
+
+            dogowner = "Dawn";
+            catowner = "Nathan";
+            Console.WriteLine( "{1} owns {0}\n{2} owns {3}" , dog, dogowner, catowner, cat );
+            Console.ReadLine();
+
+
+            Console.WriteLine()
+
+        }
+    }
+}
---
+++ 0.04_Conditional_If/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _0._04_Conditional_If
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isOn = true;
+            bool isHot = false;
+
+            /*
+             Comparison operators
+             == is equal to
+             >- Greater than
+             <- Less than
+             != Not Equal
+             */
+
+            //if (isOn) //if whats in between ()parenthases is true then run code.
+            //{
+            //    Console.WriteLine("The light is on.");
+            //}
+
+            //Console.ReadLine();
+
+            //    bool admin = true;
+            //    bool user = false;
+
+            //    if (admin)
+            //    {
+            //        Console.WriteLine("Welcome to the site Admin.");
+            //    }
+            //    if (!admin)
+            //    {
+            //        Console.WriteLine("Go home noob!");
+            //    }
+
+            //    Console.ReadLine();
+            //}
+
+            bool admin = true;
+            bool user = false;
+            bool isLoggedIn = true;
+
+            if (admin && isLoggedIn)
+            {
+                Console.WriteLine("Welcome to the site Admin.");
+            }
+            if (!admin)
+            {
+                Console.WriteLine("Go home noob!");
+            }
+
+            Console.ReadLine();
+
+        }
+
+
+
+
+}
